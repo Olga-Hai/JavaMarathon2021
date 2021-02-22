c048fd75721ede11acd60d22a0ce918dc57f1efd
dLWBDswp <olga.hayday@gmail.com>
2021-02-22 15:53:02 +0200

    Add Task1 solution

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,13 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
+        int k = 9;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            int result = i * k;
+            System.out.println(i + " x " + k + " = " + result);
+        }
 
     }
 }
