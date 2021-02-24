64f90fb100efba0182dd2bf60651394096c891ee
dLWBDswp <olga.hayday@gmail.com>
2021-02-24 15:01:39 +0200

    Add Task3 solution

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+
+        int i = 0;
+
+        while (i < 4) {
+            Scanner s = new Scanner(System.in);
+            double x = s.nextDouble();
+            double y = s.nextDouble();
+            if (y == 0) continue;
+            System.out.println(x / y);
+            i++;
+        }
 
     }
 }
